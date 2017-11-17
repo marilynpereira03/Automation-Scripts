95c5fcdefe3961f102211eebadd790d89e6942dd
avinashvazratkar <avinashvazratkar446022@gmail.com>
2017-11-17 17:59:51 +0530

    Test purpose...

---
+++ AutomatedScripts/temp.sh
@@ -0,0 +1,23 @@
+filename=credential.json
+#parse region name
+region=$(jq .region[] $filename)
+
+set -f
+  regionArray=(${region//,/ })
+  for i in "${!regionArray[@]}"
+  do
+      regionArray[$i]=$(echo ${regionArray[i]} | sed 's/","/,/g; s/^"\|"$//g')
+      echo ${regionArray[i]}
+  done
+
+  total_instance=0
+  count=$(jq .count[] $filename)
+
+  countArray=(${count//,/ })
+  for i in "${!countArray[@]}"
+  do
+    echo ${countArray[$i]}
+    total_instance=$(( $total_instance + ${countArray[$i]} ))
+
+  done
+  echo "total_instance $total_instance"
